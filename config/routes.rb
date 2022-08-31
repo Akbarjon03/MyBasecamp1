d99d2eb84d1454a1436a349d1f1b46cb957255d1
Akbarjon03 <akbarjonodilov03@gmail.com>
2022-08-30 13:43:35 -1200

    My Basecamp 1

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/about'
+  resources :projects
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
